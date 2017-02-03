a0430c9101849c8fcf62df6773ff2b5ce0e22d93
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2017-02-04 01:01:48 +0200

    Cleaned install script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Runs the speak_easy model.
 
-TRAIN_DIR='../vauvafi-crawler/output/train'
-DATA_DIR='../vauvafi-crawler/output/data'
-LOG_DIR='../vauvafi-crawler/output/log'
 ROBOT_NAME=MARVIN
 
-
-NUM_LAYERS=3
-SIZE=762
-VOCAB_SIZE=1000
-MAX_TRAIN_DATA_SIZE=0
-
-venv/bin/python speak_easy.py --num_layers $NUM_LAYERS --size $SIZE --vocab_size $VOCAB_SIZE ---max_train_data_size $MAX_TRAIN_DATA_SIZE --train_dir $TRAIN_DIR --data_dir $DATA_DIR --log_dir $LOG_DIR $@
+venv/bin/python speak_easy.py \
+  --num_layers 3 \
+  --size 762 \
+  --vocab_size 1000 \
+  ---max_train_data_size 0 \
+  --train_dir '../vauvafi-crawler/output/train' \
+  --data_dir '../vauvafi-crawler/output/data' \
+  --log_dir '../vauvafi-crawler/output/log' \
+  $@
